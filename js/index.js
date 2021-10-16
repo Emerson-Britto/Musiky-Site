ef53ad220e888b04234ed2d2036f343620fd7bbe
Emerson_Britto <emersonbritto987@gmail.com>
2021-10-16 03:52:17 -0300

    responsively page

--- js/index.js
+++ js/index.js
@@ -1,11 +1,19 @@
-let btn_open_menu = document.getElementById('menu_icon')
-let btn_close_menu = document.getElementById('menu_icon-opened')
-let nav_aside = document.querySelector('.nav_aside')
+let btn_open_menu = document.getElementById('menu_icon');
+let btn_close_menu = document.getElementById('menu_icon-opened');
+let nav_aside = document.querySelector('.nav_aside');
+let overlay = document.getElementById('overlay');
 
 btn_open_menu.addEventListener('click', ()=> {
-	nav_aside.classList.toggle('menu_opened')	
+	nav_aside.classList.toggle('menu_opened');
+	overlay.style.display = "block";
 })
 
-btn_close_menu.addEventListener('click', ()=> {
-	nav_aside.classList.toggle('menu_opened')	
-})
+const closeMenu = () => {
+	nav_aside.classList.toggle('menu_opened');
+	overlay.style.display = "none";	
+}
+
+btn_close_menu.addEventListener('click', ()=> closeMenu())
+
+overlay.addEventListener('click', ()=> closeMenu())
+
